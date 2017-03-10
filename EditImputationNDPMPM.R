2f5e02adbe371f378cfeb4b6b7311f9301f625a4
Olanrewaju Akande <akandelanre13@gmail.com>
2017-03-10 09:45:03 -0500

    Model witn n=5000

--- EditImputationNDPMPM.R
+++ EditImputationNDPMPM.R
@@ -33,9 +33,9 @@ House$TEN[which(House$TEN == 2)] <- 1
 House$TEN[which(House$TEN == 3)] <- 2
 
 
-###### 4: Take a sample of size 1500 Households
+###### 4: Take a sample of size 5000 Households
 set.seed(4321)
-sample_size <- 1500
+sample_size <- 5000
 samp_index <- sort(sample(1:nrow(House),sample_size,replace=F))
 House <- House[samp_index,]
 
